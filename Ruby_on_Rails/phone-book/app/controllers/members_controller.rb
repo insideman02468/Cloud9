6173c93c691a5ea3632a3ef36476fa1d15efdc71
EC2 Default User <ec2-user@ip-172-31-13-229.us-east-2.compute.internal>
2018-08-12 10:14:00 +0000

    リソースの編集画面を作って、編集処理を実装するを完了しました。

--- Ruby_on_Rails/phone-book/app/controllers/members_controller.rb
+++ Ruby_on_Rails/phone-book/app/controllers/members_controller.rb
@@ -1,15 +1,34 @@
 class MembersController < ApplicationController
   def index
     @members = Member.all
-  end  
+  end
   
   def new
     @member = Member.new
   end
   
   def create
-  @member = Member.new(params.require(:member).permit(:name, :yomi, :phone))
-  @member.save
+    @member = Member.new(member_params)
+    @member.save
+    redirect_to members_path
   end
   
+  def edit
+    @member = find_member_by_id
+  end
+  
+  def update
+    @member = find_member_by_id
+    @member.update(member_params)
+    redirect_to members_path
+  end
+  
+  private
+  def member_params
+    params.require(:member).permit(:name, :yomi, :phone)
+  end
+  
+  def find_member_by_id
+    Member.find(params[:id])
+  end
 end
